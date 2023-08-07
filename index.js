596488f5aab5bc4a4e05c05f209673b24815570a
vivektarun <vivektarun1234@gmail.com>
2023-08-07 18:36:35 +0530

    print habitable planet

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const isHabitablePlanets = []
+const habitablePlanets = []
 
 function isHabitablePlanet(planet) {
     return planet['koi_disposition'] === 'CONFIRMED'
-        && planet['koi_insol'] >0.36 && planet['koi_insol']< 1.11
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
         && planet['koi_prad'] < 1.6;
 
 }
@@ -17,16 +17,19 @@ fs.createReadStream('kepler_data.csv')
     }))
     .on('data', (data) => {
         if (isHabitablePlanet(data)) {
-            isHabitablePlanets.push(data);
+            habitablePlanets.push(data);
         }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        if(isHabitablePlanet.length === 1 ) {
-            console.log(`${isHabitablePlanet.length} habitable planet found!`);
+        console.log(habitablePlanets.map((planet) => { //Doubt -> How to use map function.
+            return ['kepler_name'];
+        }));
+        if (habitablePlanets.length === 1) {
+            console.log(`${habitablePlanets.length} habitable planet found!`);
         } else {
-            console.log(`${isHabitablePlanet.length} habitable planets found!`);
+            console.log(`${habitablePlanets.length} habitable planets found!`);
         }
     })
